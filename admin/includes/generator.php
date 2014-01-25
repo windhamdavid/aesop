e2e68e3508434fd191d3809f358874dbce9bbee7
windhamdavid <david@davidawindham.com>
2014-01-25 09:02:49 -0500

    bug critical

--- admin/includes/generator.php
+++ admin/includes/generator.php
@@ -1,7 +1,7 @@
 <?php
 
 	// Start WordPress
-	require_once( '../../../../../wp-load.php' );
+	require_once( '../../../../../wp/wp-load.php' );
 
 	// Capability check
 	if ( !current_user_can( 'publish_posts' ) )
